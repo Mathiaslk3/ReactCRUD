f48e524fa2fecb85816bfa94bae2e94b2ffe85e0
Mathias Lau Kjeldsen <mathiaslk3@outlook.dk>
2024-05-06 13:32:54 +0200

    Update App.jsx
    dfgd

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Users from './components/Users'
+import "./App.css";
+import Users from "./components/Users";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <Users/>
+      <Users />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
